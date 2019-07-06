a06611351c8bf33f0eef5aa4dcdac9acc23f49f9
Jorge D'Angelo <jgdfox@gmail.com>
2019-07-06 19:18:57 -0300

    first commit

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+}
